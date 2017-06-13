65d7eac238ce9599f7e7c3f7c280888ec0558f3d
20161104607 <a20161104607@admindeiMac-35.local>
2017-06-13 09:40:47 +0800

    sort
    改指针读取文件

--- sort/main.c
+++ sort/main.c
@@ -12,10 +12,17 @@ int main()
     {
     int i,j,temp;
     int a[10];
-    printf("input 10 numbers:\n");
+    FILE *fp1,*fp2;
+        fp1=fopen("//Users//a20161104607//Desktop//sort//numberout.txt ","w");
+        fp2=fopen("//Users//a20161104607//Desktop//sort//number.txt ","r");
+        if(fp2==NULL)
+        {
+            printf("打开文件错误");
+        }
+   //printf("input 10 numbers:\n");
     for(i=0;i<10;i++)
-    {
-        scanf("%d",&a[i]);
+        {
+        fscanf(fp2,"%d",&a[i]);
     }
     for(j=0;j<=9;j++)
     {
@@ -31,9 +38,13 @@ int main()
     }
         for(i=0;i<10;i++)
         {
-    printf("%d\t",a[i]);
+            printf("%d\t",a[i]);
+            fprintf(fp1,"%d ",a[i]);
+            printf(" ");
         }
     printf("\n");
+    fprintf(fp1,"\n");
     return 0;
     
     }
+
